a27511c6e75a9f3486de731e765315cf5921da0a
Vumanhcuongdhv <vuucuong1409@gmail.com>
2024-11-28 15:09:45 +0700

    Create 4.1.py

---
+++ Bai 4/4.1.py
@@ -0,0 +1,8 @@
+print("Sinh vien: Vu Manh Cuong")
+print("MSV: 235752021610030")
+# Nhập chuỗi từ người dùng
+S = input("Nhập chuỗi S: ")
+
+# In từng ký tự trên một dòng
+for char in S:
+    print(char)
