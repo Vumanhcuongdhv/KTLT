fd6c9e024df2495f851edf803327c1539ffea877
Vumanhcuongdhv <vuucuong1409@gmail.com>
2024-11-28 14:57:09 +0700

    Create 2.1.py

---
+++ Bai 2/2.1.py
@@ -0,0 +1,8 @@
+print("Sinh vien: Vu Manh Cuong")
+print("MSV: 235752021610030")
+n1 = int(input("Enter n1 value: "))
+n2 = int(input("Enter n2 value: "))
+if n1 > n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
