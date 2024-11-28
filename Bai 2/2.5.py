35e74cd6a369f6bfd7043fd74cdc69804c4591f4
Vumanhcuongdhv <vuucuong1409@gmail.com>
2024-11-28 15:00:46 +0700

    Add files via upload

---
+++ Bai 2/2.5.py
@@ -0,0 +1,9 @@
+print("Vu Manh Cuong")
+print("MSV: 235752021610030")
+# Nhập một số tự nhiên n > 0 từ người dùng
+n = int(input("Enter a number (n > 0): "))
+
+# Lặp và in các số giảm dần từ n đến 0, mỗi số in trên một hàng
+while n >= 0:
+    print(n)
+    n = n - 1  # Giảm giá trị của n đi 1 sau mỗi lần lặp
