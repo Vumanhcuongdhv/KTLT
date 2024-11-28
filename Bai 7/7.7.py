208a52495fcfbc4e3ab9539a8cfdc7be6281f85e
Vumanhcuongdhv <vuucuong1409@gmail.com>
2024-11-28 19:25:50 +0700

    Update 7.7.py

--- Bai 7/7.7.py
+++ Bai 7/7.7.py
@@ -1,2 +1,8 @@
 print("Vũ Mạnh Cường")
 print("235752021610030")
+def count_lines_in_file(filename):
+    with open(filename, 'r') as file:
+        return sum(1 for line in file) 
+filename = 'test.txt'
+line_count = count_lines_in_file(filename)
+print(f"Số dòng trong tệp '{filename}' là: {line_count}")
