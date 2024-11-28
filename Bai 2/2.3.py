3c9219072fcc9ee780562a7ae19f71ec27080a69
Vumanhcuongdhv <vuucuong1409@gmail.com>
2024-11-28 14:58:28 +0700

    Add files via upload

---
+++ Bai 2/2.3.py
@@ -0,0 +1,10 @@
+print("Vu Manh Cuong")
+print("MSV: 235752021610030")
+ # Nhập một số từ người dùng
+n = int(input("Enter a number: "))
+
+# Kiểm tra số chẵn hay lẻ
+if n % 2 == 0:
+    print("EVEN Number")  # In ra nếu số là chẵn
+else:
+    print("ODD Number")   # In ra nếu số là lẻ
